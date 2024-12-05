c6268dddbb5a9afda71a28f24066c996289d74eb
Suleman <msulsaleem@gmail.com>
2024-12-04 22:19:57 -0600

    add main spreadsheet logic

--- main.js
+++ main.js
@@ -29,3 +29,74 @@ const calc = () => {
 
     $scope.worker.postMessage($scope.sheet);
 };
+
+const Spreadsheet = ($scope) => {
+    init();
+
+    for (let col = "A"; col <= "Z"; col = String.fromCharCode(col.charCodeAt() + 1)) {
+        let th = document.createElement("th");
+        th.textContent = col;
+        document.querySelector("tr").appendChild(th);
+        $scope.Cols.push(col);
+    }
+
+    for (let row = 1; row <= 100; row++) {
+        $scope.Rows.push(row);
+    }
+
+    $scope.Rows.forEach((row) => {
+        let th = document.createElement("th");
+        th.innerHTML = row;
+        let tr = document.createElement("tr");
+        tr.appendChild(th);
+
+        $scope.Cols.forEach((col) => {
+            let td = document.createElement("td");
+            tr.appendChild(td);
+            let input = document.createElement("input");
+            input.setAttribute("id", col+row);
+
+            if (!((col+row) in $scope.sheet)) {
+                $scope.sheet[col+row] = "";
+            }
+
+            for (let event of ["change", "input", "paste"]) {
+                input.addEventListener(event, () => {
+                    $scope.sheet[col+row] = input.value;
+                    calc();
+                });
+            }
+
+            input.addEventListener("keydown", function(event) {
+                // switch (event.key) {
+                //     case "ArrowUp":
+                //     case "ArrowDown":
+                //     case "Enter":
+                //         let vdirection = (event.key === "ArrowUp" ? -1 : 1);
+                //         (document.querySelector("#"+col+(row+vdirection)) || event.target).focus();
+                //     case "ArrowLeft":
+                //     case "ArrowRight":
+                //         let hdirection = (event.key === "ArrowLeft" ? -1: 1);
+                //         (document.querySelector("#"+(col+hdirection)+row) || event.target).focus(); 
+
+                // }
+                if (event.key === "ArrowUp" || event.key === "ArrowDown" || event.key === "Enter") {
+                    let direction = (event.key === "ArrowUp" ? -1 : 1);
+                    (document.querySelector("#"+col+(row+direction)) || event.target).focus();
+                }
+
+                if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
+                    let direction = (event.key === "ArrowLeft" ? -1 : 1);
+                    (document.querySelector("#"+(String.fromCharCode(col.charCodeAt() + direction))+row) || event.target).focus();
+                }
+            });
+
+            let div = document.createElement("div");
+            div.setAttribute("id", "_"+col+row);
+            td.appendChild(input);
+            td.appendChild(div);
+        });
+
+        document.querySelector("table").appendChild(tr);
+    });
+}
